2e038503dc9dfe92efce05b8a9f15ba6e8bdc26e
SirQuandale <302685723@student.rocmondriaan.nl>
2022-11-23 14:12:20 +0100

    added querySelectors

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.intro').textContent = "Dit is mijn eerste querySelector!";
+document.querySelector('.blog').textContent = "Hier schrijf is mijn blog artikel";
+document.querySelector('.mix').textContent = "<h1>Deze tekst bevat ook een h element</h1>";
+document.querySelector('.styling').innerHTML = "<p>Dit is een element met styling</p>"
+document.querySelector('.styling').style.background = "violet";
